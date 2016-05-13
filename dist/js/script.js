d588587f4c5a521c118f50b402ca8d17371b425b
AntonEssential <antonessential@hotmail.co.uk>
2016-05-13 18:44:42 +0300

    verison 1.11

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,7 +11,24 @@
 
   // Navgoco acordion
   $(".catalog__acordion").navgoco({accordion: true});
-
+  $(".acord-main").navgoco({accordion: true});
+  // $(".acord__item").click(function(event) {
+  //   $('.acord__item').addClass('open')
+  //   $(this).addClass('open')
+  // });
+  $('.acord__link').click(function(event) {
+    $('.acord__item-2').removeClass('jsAcordActive')
+    if ($('.acord__item').hasClass('open')) {
+      $('.b-mac').addClass('fadeOut')
+    }
+    else {
+      $('.b-mac').removeClass('fadeOut')
+    }
+  });
+  $('.acord__item-2').click(function(event) {
+    $('.acord__item-2').removeClass('jsAcordActive')
+    $(this).addClass('jsAcordActive')
+  });
   // jQuery price slider
   $(function() {
     $( "#slider-range" ).slider({
